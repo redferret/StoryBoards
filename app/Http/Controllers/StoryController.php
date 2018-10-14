4088a5a9a9ee047b3155c4e89f2804815e276a09
Richard DeSilvey <rdesilvey@gmail.com>
2018-10-14 14:55:15 -0600

    StoryController

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -2,84 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Story;
 use Illuminate\Http\Request;
 
-class StoryController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+class StoryController extends Controller {
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct() {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function getAll() {
+    $stories = Auth::user()->stories;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Story  $story
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Story $story)
-    {
-        //
+    foreach ($stories as $story) {
+      $story->pages;
     }
+    return $stories;
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Story  $story
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Story $story)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request) {
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Story  $story
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Story $story)
-    {
-        //
-    }
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Story  $story
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Story $story)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request) {
+
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Page  $page
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Page $page) {
+
+  }
 }
